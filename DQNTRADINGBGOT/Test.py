1bb5cddeffaf07e9d10085ca85e37c380fbd59d1
Steffen pedersen <stped18@student.sdu.dk>
2021-01-30 07:01:50 +0100

    reward list add

--- DQNTRADINGBGOT/Test.py
+++ DQNTRADINGBGOT/Test.py
@@ -7,6 +7,7 @@ if __name__=="__main__":
     data= env.scalleDate()
     state = data.iloc[0]
     agent = Agent(n_actions=6, batch_size=64, epsilon=1.00, input_dims=len(state), lr=0.0005, gamma=0.99)
+    reward_list=[]
     while True:
         print("Starter")
         for index, row in data.iterrows():
@@ -14,19 +15,24 @@ if __name__=="__main__":
             state=env.Update(state)
             action = agent.act(state)
             reward=env.episode(action=action, observation=state)
+            reward_list.append(reward)
             agent.observe(state=state, action=action,reward=reward,new_state=row,done=False)
             state=row
             agent.DQN_learn()
             if index%60==0:
+                print("ballance :{0} equent :{1} Profir :{2} .reward {3}, action {4} ".format(env.acount.balance,
+                                                                                              env.acount.equant,
+                                                                                              env.acount.profit, sum(reward_list),
+                                                                                              action))
+                reward_list=[]
+                agent.observe(state=state, action=action, reward=reward, new_state=row, done=True)
+                agent.save_model()
                 agent.PG_learn()
             if env.acount.balance<0:
                 agent.observe(state=state, action=action, reward=reward, new_state=row, done=True)
                 agent.DQN_learn()
                 break
-        print("ballance :{0} equent :{1} Profir :{2} .reward {3}, action {4} ".format(env.acount.balance,
-                                                                                      env.acount.equant,
-                                                                                      env.acount.profit, reward,
-                                                                                      action))
+
         agent.observe(state=state, action=action, reward=reward, new_state=row, done=True)
         agent.DQN_learn()
         agent.save_model()
